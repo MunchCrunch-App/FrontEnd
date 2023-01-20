dfdea6c0b8dcc052136cd50bba81a3d76ac07373
ksu930 <ksu930310@gmail.com>
2023-01-20 15:15:17 +0900

    추천 페이지 구현중

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
   <div
     class="flex justify-center items-center w-full min-h-screen bg-slate-400"
   >
-    <Layout>
-      <RouterView />
-    </Layout>
+    <RouterView />
   </div>
 </template>
 
 <script>
 import { RouterView } from "vue-router";
-import Layout from "./components/global/Layout.vue";
+
 export default {
-  components: {
-    Layout: Layout,
-  },
+  components: {},
 };
 </script>
---
+++ src/components/home/globalHome/TimeAttack.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flex justify-start w-full h-[180px] my-8 box-border">
+    <div class="flex w-full h-full box-border px-5">
+      <swiper
+        class="h-full w-full"
+        :slidesPerView="4"
+        :spaceBetween="20"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+        @touchStart="(swiper, event) => event.stopPropagation()"
+      >
+        <swiper-slide>
+          <div
+            class="flex flex-col items-center justify-center h-full rounded-[32px] bg-beGreen box-border"
+          >
+            <img
+              class="w-[52px] h-[52px] mb-5"
+              src="../../../assets/no-pictures.png"
+            />
+            <div class="text-lg font-bold">타임어택</div>
+            <div class="text-lg">마지막 1명!</div>
+          </div>
+        </swiper-slide>
+        <swiper-slide
+          v-for="timeAttackItem in timeAttackLists"
+          :key="timeAttackItem.id"
+        >
+          <img
+            class="w-full h-32"
+            :src="timeAttackItem.imageUrl"
+            alt="timeAttackItem"
+          />
+          <div
+            class="flex w-full overflow-hidden text-ellipsis whitespace-nowrap"
+          >
+            {{ timeAttackItem.title }}
+          </div>
+          <div
+            class="flex items-center justify-center text-xl font-bold text-beRed"
+          >
+            {{ timeAttackItem.price.toLocaleString() }}원
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+export default {
+  props: {
+    timeAttackLists: Array,
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onSwiper(swiper) {},
+    onSlideChange(swiper) {},
+    onClickTab(index) {},
+  },
+};
+</script>
+
+<style></style>
