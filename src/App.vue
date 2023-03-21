b4cafa2ded2c38b5515daf1509a2ed0f4c1818f1
ksu930 <ksu930310@gmail.com>
2023-03-21 11:09:10 +0900

    트랜지션 적용

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <RouterView v-slot="{ Component }">
         <Transition name="route" mode="out-in">
             <div
-                class="relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-slate-400 font-pretendard"
+                class="relative flex min-h-screen w-full items-center justify-center overflow-x-hidden overflow-y-hidden bg-slate-400 font-pretendard scrollbar-hide"
                 :key="$route.name"
             >
                 <component :is="Component"></component>
@@ -22,13 +22,19 @@ export default {
     transform: translateX(100%);
 }
 .route-enter-active {
-    transition: ass 0.3s ease-out;
+    transition: opacity 0.2s ease;
+}
+.route-enter-to {
+    opacity: 1;
 }
 .route-leave-to {
     opacity: 0;
     transform: translateX(-100%);
 }
 .route-leave-active {
-    transition: all 0.3s ease-in;
+    transition: all 0.5s ease;
+}
+.route-leave-from {
+    opacity: 1;
 }
 </style>
