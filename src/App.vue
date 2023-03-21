106377adb2cd69ee3716a5ff3d787f9a2f78eb8d
ksu930 <ksu930310@gmail.com>
2023-03-21 15:32:31 +0900

    트랜지션 완료

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <RouterView v-slot="{ Component }">
-        <Transition name="route" mode="out-in">
+        <Transition :name="transition" mode="in-out">
             <div
-                class="relative flex min-h-screen w-full items-center justify-center overflow-x-hidden overflow-y-hidden bg-slate-400 font-pretendard scrollbar-hide"
+                class="fixed flex min-h-screen w-full items-center justify-center overflow-x-hidden overflow-y-hidden bg-slate-400 font-pretendard scrollbar-hide"
                 :key="$route.name"
             >
                 <component :is="Component"></component>
@@ -13,28 +13,65 @@
 
 <script>
 export default {
-    components: {},
+    data() {
+        return {
+            transition: '',
+        };
+    },
+    watch: {
+        $route(to, from) {
+            const toDepth = to.path.split('/').length;
+            const fromDepth = from.path.split('/').length;
+            if (toDepth === fromDepth) this.transition = 'slide-up';
+            if (toDepth < fromDepth) this.transition = 'slide-right';
+            if (toDepth > fromDepth) this.transition = 'slide-left';
+
+            console.log(this.transition);
+        },
+    },
 };
 </script>
 <style>
-.route-enter-from {
-    opacity: 0;
+.transition {
+    overflow: hidden;
+}
+
+.slide-left-enter-active {
+    transition: all 0.3s ease-in-out;
+    z-index: 2;
     transform: translateX(100%);
 }
-.route-enter-active {
-    transition: opacity 0.2s ease;
+.slide-left-enter-to {
+    z-index: 2;
+    transform: translateX(0%);
 }
-.route-enter-to {
-    opacity: 1;
-}
-.route-leave-to {
-    opacity: 0;
+
+.slide-right-enter-active {
+    transition: all 0.3s ease-in-out;
+    z-index: 2;
     transform: translateX(-100%);
 }
-.route-leave-active {
-    transition: all 0.5s ease;
+.slide-right-enter-to {
+    z-index: 2;
+    transform: translateX(0%);
 }
-.route-leave-from {
+
+.slide-up-enter-active {
+    transition: all 0.1s ease-in;
+    z-index: 2;
+    opacity: 0;
+}
+.slide-up-enter-to {
+    z-index: 2;
     opacity: 1;
+}
+
+.slide-left-leave-active.slide-right-leave-active.slide-up-leave-active {
+    transition: opacity 0.1s ease;
+    z-index: -1;
+}
+.slide-left-leave-to.slide-right-leave-to.slide-up-leave-to {
+    opacity: 0;
+    z-index: -1;
 }
 </style>
